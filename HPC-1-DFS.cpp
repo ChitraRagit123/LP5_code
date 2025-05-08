57f2989ba96f401b85ee1557486b743e0ef03054
Chitra Ragit <113353595+ChitraRagit123@users.noreply.github.com>
2025-05-08 13:07:14 +0530

    Update HPC-1-DFS.cpp

--- HPC-1-DFS.cpp
+++ HPC-1-DFS.cpp
@@ -7,56 +7,50 @@ const int MAX = 100000;
 vector<int> graph[MAX];
 bool visited[MAX];
 
-// Recursive parallel DFS
 void dfs(int node) {
-    #pragma omp critical
-    {
-        if (visited[node]) return;
-        visited[node] = true;
-        cout << node << " ";
-    }
+    // Mark the node as visited
+    visited[node] = true;
+    cout << node << " ";
 
-    // Parallelize the neighbor traversal
-    #pragma omp parallel for
-    for (int i = 0; i < graph[node].size(); i++) {
-        int neighbor = graph[node][i];
-        if (!visited[neighbor]) {
-            dfs(neighbor);
+    // Parallelizing the recursive DFS calls for each unvisited neighbor using OpenMP tasks
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for (int i = 0; i < graph[node].size(); i++) {
+                int adj_node = graph[node][i];
+                // If the neighbor hasn't been visited, create a task to explore it
+                if (!visited[adj_node]) {
+                    #pragma omp task
+                    dfs(adj_node);
+                }
+            }
         }
     }
 }
 
 int main() {
     int n, m, start_node;
-    cout << "Enter number of nodes, edges and start node: ";
+    cout << "Enter number of nodes, edges, and start node: ";
     cin >> n >> m >> start_node;
 
-    cout << "Enter pairs of edges:\n";
+    cout << "Enter pairs of edges:" << endl;
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         graph[u].push_back(v);
-        graph[v].push_back(u); // Undirected graph
+        graph[v].push_back(u); // undirected graph
     }
 
-    // Initialize visited array in parallel
+    // Initialize visited array
     #pragma omp parallel for
     for (int i = 0; i < n; i++) {
         visited[i] = false;
     }
 
     cout << "DFS Traversal: ";
-
-    // Start DFS in parallel region
-    #pragma omp parallel
-    {
-        #pragma omp single
-        {
-            dfs(start_node);
-        }
-    }
-
+    dfs(start_node);
     cout << endl;
+
     return 0;
 }
-
